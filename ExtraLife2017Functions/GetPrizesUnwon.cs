1d2d1f5c626def3379e39c58b4981a867d11f36e
ssfcultra <koolkeith13@gmail.com>
2017-11-04 22:23:28 -0700

    Added GetPrizesUnwon API

--- ExtraLife2017Functions/GetPrizesUnwon.cs
+++ ExtraLife2017Functions/GetPrizesUnwon.cs
@@ -1,32 +1,33 @@
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using ExtraLife2017Functions.Interfaces;
 using ExtraLife2017Functions.Models;
 using ExtraLife2017Functions.Repositories;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-using System.Collections.Immutable;
-using System.Net;
-using System.Net.Http;
 
 namespace ExtraLife2017Functions
 {
-    public static class GetPrizes
+    public static class GetPrizesUnwon
     {
-        [FunctionName("GetPrizes")]
+        [FunctionName("GetPrizesUnwon")]
 
         public static HttpResponseMessage Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Prizes")]
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Prizes/Unwon")]
             HttpRequestMessage req,
             TraceWriter log)
         {
             log.Info("GetPrizes HTTP trigger function processed a request.");
 
             IPrizeRepository repository = new PrizeRepository();
-            var prizes = repository.RetrieveAsync().Result.ToImmutableList();
+            var prizes = repository.RetrieveAsync().Result.Where(prize => string.IsNullOrEmpty(prize.WonBy)).ToImmutableList();
             var builder = ImmutableList.CreateBuilder<Prize>();
             builder.AddRange(prizes);
-
-            // Fetching the name from the path parameter in the request URL
+            
             return req.CreateResponse(HttpStatusCode.OK, builder);
         }
     }
